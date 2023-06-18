ae5e2d910ba440db230654bab83d427e36316b3a
sesa684280 <dave.dhaiti@se.com>
2023-06-18 21:03:24 +0200

    engancedRGB

--- OverQuest/Assets/Menu/Scripts/RGB.cs
+++ OverQuest/Assets/Menu/Scripts/RGB.cs
@@ -9,10 +9,23 @@ public class RGB : MonoBehaviour
     public Slider greenSlider;
     public Slider blueSlider;
     public Image background;
+    public Image otherBackground;
+
+    private const string RedKey = "RedValue";
+    private const string GreenKey = "GreenValue";
+    private const string BlueKey = "BlueValue";
+
 
     private void Start()
     {
-        // Ajouter des écouteurs d'événements pour les sliders
+        if (PlayerPrefs.HasKey(RedKey))
+        {
+            redSlider.value = PlayerPrefs.GetFloat(RedKey);
+            greenSlider.value = PlayerPrefs.GetFloat(GreenKey);
+            blueSlider.value = PlayerPrefs.GetFloat(BlueKey);
+            UpdateBackgroundColor();
+        }
+        // Parametrage à partir des sliders
         redSlider.onValueChanged.AddListener(UpdateBackgroundColor);
         greenSlider.onValueChanged.AddListener(UpdateBackgroundColor);
         blueSlider.onValueChanged.AddListener(UpdateBackgroundColor);
@@ -20,15 +33,35 @@ public class RGB : MonoBehaviour
 
     private void UpdateBackgroundColor(float value)
     {
+        // Sauvegarder les valeurs des sliders
+        PlayerPrefs.SetFloat(RedKey, redSlider.value);
+        PlayerPrefs.SetFloat(GreenKey, greenSlider.value);
+        PlayerPrefs.SetFloat(BlueKey, blueSlider.value);
+        PlayerPrefs.Save();
+
+        // Mettre à jour la couleur de l'arrière-plan
+        UpdateBackgroundColor();
+    }
+
+    private void UpdateBackgroundColor()
+    {
         // Obtenir les valeurs des sliders
-        float redValue = redSlider.value ;
-        float greenValue = greenSlider.value ;
-        float blueValue = blueSlider.value ;
+        float redValue = redSlider.value;
+        float greenValue = greenSlider.value;
+        float blueValue = blueSlider.value;
 
-        Debug.Log("r: " + redSlider.value + "g: " + greenSlider.value + "b: " + blueSlider.value);
-
-        // Mettre à jour la couleur de l'arrière-plan
-        Color newColor = new Color(redValue  , greenValue  , blueValue)  ;
+        // Mettre à jour la couleur de l'arrière-plan
+        Color newColor = new Color(redValue, greenValue, blueValue);
         background.color = newColor;
+    }
+
+    private void UpdateOtherBackgroundColor()
+    {
+        float redValue = redSlider.value;
+        float greenValue = greenSlider.value;
+        float blueValue = blueSlider.value;
+
+        Color newColor = new Color(redValue, greenValue, blueValue);
+        otherBackground.color = newColor;
     }
 }
